51cc9d441dd8b38c62cd834185d068f00edfefee
binpatel31 <binpatel31@gmail.com>
2020-04-16 20:05:19 -0400

    xm.c

--- paging/xm.c
+++ paging/xm.c
@@ -14,24 +14,43 @@
  */
 SYSCALL xmmap(int virtpage, bsd_t source, int npages)
 {
- // kprintf("xmmap - to be implemented!\n");
-  STATWORD ps;
-  disable(ps);
+        STATWORD ps;
+        disable(ps);
 
-  if(virtpage<(TWOTEN * 4) || source<(TWOTEN - TWOTEN) || source>=(TWOTEN / 128) || npages<(TWOTEN / TWOTEN) || npages>(TWOTEN / 4)){
-  	restore(ps);
-	return SYSERR;
-  }
-  if(bsm_tab[source].bs_private==SETONE){
-  	restore(ps);
-	return SYSERR;
-  }
-  if(bsm_tab[source].bs_mapping>SETZERO && npages> bsm_tab[source].bs_npages){
-  	restore(ps);
-	return SYSERR;
-  }
+	if(source<0 || source>=16)
+	{
+                kprintf("2");
+                restore(ps);
+                return SYSERR;		
+	}
+        if(npages<1 || npages>128)
+        {
+                kprintf("3");
+                restore(ps);
+                return SYSERR;
+        }
 
-  bsm_map(currpid,virtpage,source,npages);
+	if(virtpage<(TWOTEN * 4))
+	{
+                kprintf("4");
+  		restore(ps);
+		return SYSERR;
+	}
+
+  	if(bsm_tab[source].bs_private==1)
+	{
+    		kprintf("5"); 
+  		restore(ps);
+		return SYSERR;
+  	}
+  	if(npages> bsm_tab[source].bs_npages)
+	{
+		    kprintf("6");  
+  		restore(ps);
+		return SYSERR;
+  	}
+
+  	bsm_map(currpid,virtpage,source,npages);
 
   restore(ps);
   return OK;
@@ -45,16 +64,21 @@ SYSCALL xmmap(int virtpage, bsd_t source, int npages)
  */
 SYSCALL xmunmap(int virtpage)
 {
-  //kprintf("To be implemented!");
-  STATWORD ps;
-  disable(ps);
+  	//kprintf("To be implemented!");
+ 	STATWORD ps;
+  	disable(ps);
 
-  if(virtpage<(TWOTEN * 4)){
+  	if(virtpage<4096)
+	{
+  		restore(ps);
+		return SYSERR;
+  	}
+  	int a = bsm_unmap(currpid,virtpage,0);
+        if (a!=OK)
+	{
+		restore(ps);
+		return SYSERR;
+	}
   	restore(ps);
-	return SYSERR;
-  }
-
-  bsm_unmap(currpid,virtpage);
-  restore(ps);
-  return OK;
+  	return OK;
 }
